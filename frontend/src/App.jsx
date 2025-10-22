bb5dd646ef442c901d07cfc4f4bcc7d43266d0e8
omcglynn <owenemcg@gmail.com>
2025-10-22 15:06:31 -0400

    Cleaned up code structure
    Created a views folder so we arent building the entire program in the App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from 'react'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
-import logo from './assets/ChatGraphImage1.png'
 import './App.css'
-import Homepage from './Homepage'
+import Homepage from './views/Homepage'
+import Entry from './views/Entry'
 import supabase from './supabaseClient'
 
 export default function App() {
-  const [showLogin, setShowLogin] = useState(false)
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -23,56 +22,18 @@ export default function App() {
   const handleLogout = async () => {
     await supabase.auth.signOut()
     setUser(null)
-    setShowLogin(false)
   }
-  if (user) {
+  if (!user) {
     return (
-      <div className="App">
-        <Homepage supabase={supabase} user={user} onLogout={handleLogout} />
-      </div>
+      <Entry supabase={supabase} />
     )
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to ChatGraph! Login or signup below.</p>
-        <p>Click "About" to learn more.</p>
-
-        {user ? (
-          <>
-            <p>Logged in as <strong>{user.email}</strong></p>
-            <button className="App-link" onClick={handleLogout}>
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            {!showLogin ? (
-              <>
-                <button className="App-link" onClick={() => setShowLogin(true)}>
-                  Login / Signup
-                </button>
-                <div>
-                  <a className="App-link" href="/about">
-                    About
-                  </a>
-                </div>
-              </>
-            ) : (
-              <div style={{ width: 400 }}>
-                <Auth
-                  supabaseClient={supabase}
-                  appearance={{ theme: ThemeSupa }}
-                  providers={['github', 'google']}
-                  redirectTo="http://localhost:5177"
-                />
-              </div>
-            )}
-          </>
-        )}
-      </header>
-    </div>
+      <div className="App">
+        <Homepage supabase={supabase} user={user} onLogout={handleLogout} />
+      </div>
   )
+
+  
 }

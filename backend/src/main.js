3d2e6704dcfde645dbd43724108b4fc3d4c7fbc5
omcglynn <owenemcg@gmail.com>
2025-10-22 14:46:53 -0400

    anonymized api keys

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!')
 })
 
 app.post('/', (req, res) => {
@@ -12,6 +12,10 @@ app.post('/', (req, res) => {
 
 app.put('/user', (req, res) => {
   res.send('Got a PUT request at /user')
+})
+
+app.get('/user', (req, res) => {
+  res.send('Got a GET request at /user')
 })
 
 app.delete('/user', (req, res) => {

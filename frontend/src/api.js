ba4d9c2777191e18808d347af59d32549d057f01
sammitnepal <nepalsammit@gmail.com>
2025-11-15 16:16:27 -0500

    api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,27 @@
 const API_BASE_URL= import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
 
-export async function fetchData() {
+export async function fetchData(sb,path,options={}) {
+    const {data :{session},error} = await sb.auth.getSession();
+    if (error) {
+        throw new Error('Failed to get session: ' + error.message);
+    }
+    const token = session?.access_token;
+    if(!token) console.warn("No token found in session");
+    const headers ={
+        "Content-Type": "application/json",
+        ...(options.headers || {}), 
+    }
+    if(token) headers["Authorization"] =`Bearer ${token}`;
+
+    const response = await fetch(`${API_BASE_URL}${path}`, {
+        ...options,
+        headers,
+    });
+
+    if(!response.ok){
+        throw new Error(await response.text()||"API request failed")
+    }
+    return response.json();
+
+
 }
